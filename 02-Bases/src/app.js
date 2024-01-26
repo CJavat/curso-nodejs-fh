65b8dd2ff62dd30a9670a60419e38e2dcee2ce7f
CJavatX <cdpm98@hotmail.com>
2024-01-26 11:24:25 -0600

    SECCIÓN 3 - TERMINADO

---
+++ 02-Bases/src/app.js
@@ -0,0 +1,15 @@
+// const { getUUID, getAge } = require("./plugins");
+// const { emailTemplate } = require('./js-foundation/01-template');
+// require('./js-foundation/02-destructuring');
+// console.log( emailTemplate );
+// const { buildMakePerson } = require('./js-foundation/05-Factory');
+
+// const makePerson = buildMakePerson({ getUUID, getAge });
+// const obj = { name: 'John', birthDate: '1998-05-23' };
+
+// const john = makePerson( obj );
+
+// console.log({ john });
+
+const getPokemonById = require("./js-foundation/06-promises");
+getPokemonById( 1 ).then( resp => console.log( resp ) );
