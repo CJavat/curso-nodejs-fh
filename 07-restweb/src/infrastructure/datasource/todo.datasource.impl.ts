249d9be86dca61c2cd9b847cd0beb6440792a1ce
CJavat <cdpm98@hotmail.com>
2024-02-01 07:05:31 -0600

    SECCIÓN 16 / CAP 232 - EN CURSO

---
+++ 07-restweb/src/infrastructure/datasource/todo.datasource.impl.ts
@@ -0,0 +1,49 @@
+import { prisma } from "../../data/postgres";
+import { CreateTodoDto, TodoDatasource, TodoEntity, UpdateTodoDto } from "../../domain";
+
+export class TodoDatasourceImpl implements TodoDatasource {
+  async create(createTodoDto: CreateTodoDto): Promise<TodoEntity> {
+    const todo = await prisma.todo.create({
+      data: createTodoDto!
+    });
+
+    return TodoEntity.formObject( todo );
+  }
+  
+  async getAll(): Promise<TodoEntity[]> {
+    const todos = await prisma.todo.findMany();
+
+    return todos.map( todo => TodoEntity.formObject( todo ));
+  }
+
+  async findById(id: number): Promise<TodoEntity> {
+    const todo = await prisma.todo.findFirst({
+      where: { id },
+    });
+
+    if( !id ) throw `Todo with id ${id} not found`;
+    return TodoEntity.formObject({ todo }); //! PUEDE ESTAR MAL ESTO.
+  }
+
+  async updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity> {
+    await this.findById( updateTodoDto.id );
+
+    const updatedTodo = await prisma.todo.update({
+      where: { id: updateTodoDto.id },
+      data: updateTodoDto!.values
+    });
+
+    return TodoEntity.formObject( updatedTodo );
+  }
+
+  async deleteById(id: number): Promise<TodoEntity> {
+    await this.findById( id );
+
+    const deleted = await prisma.todo.delete({
+      where: { id: id }
+    });
+
+    return TodoEntity.formObject( deleted );
+  }
+
+}
