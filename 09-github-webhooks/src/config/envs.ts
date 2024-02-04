8687e0427e4c87ae2c0a744fdccfb88b24d099b2
CJavat <cdpm98@hotmail.com>
2024-02-04 16:06:29 -0600

    SECCIÓN 24 - TERMINADO

--- 09-github-webhooks/src/config/envs.ts
+++ 09-github-webhooks/src/config/envs.ts
@@ -4,4 +4,5 @@ import { get } from 'env-var';
 export const envs = {
   PORT: get('PORT').required().asPortNumber(),
   DISCROD_WEBHOOK_URL: get('DISCROD_WEBHOOK_URL').required().asString(),
+  SECRET_TOKEN: get('SECRET_TOKEN').required().asString(),
 }
